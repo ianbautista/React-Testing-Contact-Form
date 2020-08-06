61bcc9ebdd7068bcedb5d99a625407ce47c2a875
ianbautista <ianfbautista@gmail.com>
2020-08-06 15:23:51 -0700

    simulating user inputs

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
 test("renders ContactForm without crashing", () => {
@@ -15,4 +15,12 @@ test("Adds user info when user filled out form and submitted,.", () => {
 	const inputLastName = screen.getByLabelText(/last name/i);
 	const inputEmail = screen.getByLabelText(/email/i);
 	const inputMessage = screen.getByLabelText(/message/i);
+
+	// simulating user inputs
+	fireEvent.change(inputFirstName, { target: { value: "Christian" } });
+	fireEvent.change(inputLastName, { target: { value: "Bautista" } });
+	fireEvent.change(inputEmail, { target: { value: "christian-bautista@lambdastudents.com" } });
+	fireEvent.change(inputMessage, { target: { value: "Test Message." } });
+
+	screen.debug();
 });
